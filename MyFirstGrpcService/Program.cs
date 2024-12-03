8a556348e4d1a6599f01404e8f787d8a30602e73
João Oliveira <joao.renault.oliveira@efacec.com>
2024-12-03 12:58:42 +0000

    Implements Swagger interface for testing

--- MyFirstGrpcService/Program.cs
+++ MyFirstGrpcService/Program.cs
@@ -5,10 +5,32 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddGrpc().AddJsonTranscoding();
 
+// Add Swagger/OpenAPI support
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "gRPC transcoded API",
+        Version = "v1"
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "gRPC transcoded API V1");
+    });
+}
+
+// Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/", () => Results.Redirect("/swagger"));
 
 app.Run();
